c41e8484ecabdb3247d26c7a633aefff0ca47724
TLCroatt <trishacroatt@gmail.com>
2020-07-18 09:45:55 -0600

    debug JS

--- script.js
+++ script.js
@@ -1,56 +1,63 @@
 var startButton = document.getElementById("start-btn");
-var answerButton = document.getElementById("answer-btns");
+var answerButton = document.querySelector(".answer-buttons");
 var questionContainer = document.getElementById("question-container");
 var questionElement = document.getElementById("question");
-var answerButtonsElement = document.getElementById("answer-buttons");
+var answerButtonsElement = document.querySelector(".answer-buttons");
 
-let randomQuestion, currentQuestionIndex
+let randomQuestion
+let currentQuestionIndex
 
 startButton.addEventListener("click", startQuiz);
+answerButton.addEventListener("click", selectAnswer);
 
 function startQuiz() {
     console.log("Started")
     startButton.classList.add("hide");
-    randomQuestion = questions.sort(() => Math.random() - .5);
+    //randomQuestion = questions.sort(() => Math.random() - .5);
     currentQuestionIndex = 0;
     questionContainer.classList.remove("hide");
+    showQuestion();
     function startTimer() {}
 }
 
 function setNextQuestion() {
-    showQuestion(randomQuestion[currentQuestionIndex]);
+    //showQuestion(randomQuestion[currentQuestionIndex]);
 }   
 
 function showQuestion() {
-    questionElement.innerText = question.question;
-    question.answers.forEach(answer => {
-        var button = document.createElement("button");
-        button.innerText = answer.text;
-        button.classList.add("btn");
-        if (answer.correct) {
-            button.dataset.correct = answer.correct;
+    questionElement.innerText = questions[currentQuestionIndex].question;
+    questions[currentQuestionIndex].choices.forEach((choices, choiceIndex) => {
+        var choiceBtn = document.getElementById(choiceIndex);
+        choiceBtn.innerText = choices.text;
+        choiceBtn.classList.add("choiceBtn");
+        if (choices.correct) {
+            choiceBtn.dataset.correct = choices.correct;
         }
-        button.addEventListener("click", selectAnswer);
-})
+        
+    })
+}
 
-answerButton.addEventListener("mouseover", function() {
-    this.style.backgroundColor = "#9370DB";
-})
-
-function selectAnswer() {
-
-
+function selectAnswer(event) {
+    //makes sure that nothing registers unless it's a button click
+    if (event.target.id === 0 || event.target.id) {
+        console.log(event.target.id);
+    }
+    
+    //grab id from the event and compare w/ the answer index
+//if/else for correct or wrong answer
+//update the currentQuestionIndex to next question
+//game ends if time is up or reach last question
 }
 
 var questions = [
     {
         question: "What is a boolean?",
-        answers: [
-            { text: "A data type that can only have one of two values", correct: true},
-            { text: "Zero or more characters written inside quotes", correct: false},
-            { text: "A special variable that can hold mutiple values", correct: false},
-            { text: "A tired ghost", correct: false}
-        ]
+        choices: [
+            { text: "A data type that can only have one of two values"},
+            { text: "Zero or more characters written inside quotes"},
+            { text: "A special variable that can hold mutiple values"},
+            { text: "A tired ghost"}
+        ], answerIndex: 0
             
     }
-]
+];
